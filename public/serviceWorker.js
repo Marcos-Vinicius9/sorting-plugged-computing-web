536ac739a7e51dc255787d45c1e07dbc813b6d90
Marcos Vinicius <viniciusferreirag213@gmail.com>
2023-12-28 23:58:11 -0300

    adding pwa support

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -65,7 +65,6 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       return cachedResponse || fetch(event.request).catch(() => {
-        // Se não houver resposta no cache, retorna a página inicial
         return caches.match("/index.html");
       });
     })
