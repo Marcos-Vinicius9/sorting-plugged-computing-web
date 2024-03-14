8764bc9836eb9d76d43943df9114d440cd81c928
Marcos Vinicius <viniciusferreirag213@gmail.com>
2024-03-14 19:27:16 -0300

    correção level4

--- src/screens/levels/level4/level4_page1.jsx
+++ src/screens/levels/level4/level4_page1.jsx
@@ -4,6 +4,7 @@ import level4_1 from '../../../assets/images/levels/level4/level4_1.png';
 const level4_page1 = () => {
     return (
         <LevelScreen
+            previousLevel="/LevelSelection"
             level={4}
             image={level4_1}
             description="Agora vamos comparar os métodos de Ordenação por Inserção e Quicksort."
